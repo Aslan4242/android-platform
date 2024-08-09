2fe2ac32526f0566dd1620fe99eaeecee929da51
Aslan <shkahov91@gmail.com>
2024-08-09 17:37:01 +0300

    добавил пустые строки в конце файлов

--- app/src/main/java/com/example/androidplatform/domain/api/LaunchOperationInteractor.kt
+++ app/src/main/java/com/example/androidplatform/domain/api/LaunchOperationInteractor.kt
@@ -4,6 +4,7 @@ import com.example.androidplatform.data.network.launch_operation.OperationCode
 import com.example.androidplatform.domain.models.SearchResultData
 import com.example.androidplatform.domain.models.launch_operation.OperationItem
 import kotlinx.coroutines.flow.Flow
+
 interface LaunchOperationInteractor {
     suspend fun launchOperation(operationCode: OperationCode): Flow<SearchResultData<OperationItem>>
 }
